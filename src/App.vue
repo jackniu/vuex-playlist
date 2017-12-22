3d36f5c184b1f9e7a5b7432e45e3b82c0130e57e
Niu, Chuang [MEDCN Non-J&J] <cniu5@its.jnj.com>
2017-12-22 16:04:21 +0800

    Git init.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <product-list-one :products="products"></product-list-one>
+    <product-list-two :products="products"></product-list-two>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+export default {
+  name: 'app',
+  components: {
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  data() {
+    return {
+      products:[
+        {name:"马云",price:200},
+        {name:"马化腾",price:140},
+        {name:"马冬梅",price:20},
+        {name:"马蓉",price:10},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
