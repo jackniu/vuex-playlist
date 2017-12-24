2079a90f1b5cbcc1093d07088966e143d7172f36
Niu, Chuang [MEDCN Non-J&J] <cniu5@its.jnj.com>
2017-12-24 16:44:29 +0800

    Move getters to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -11,5 +11,18 @@ export const store = new Vuex.Store({
 			{name:"马冬梅",price:20},
 			{name:"马蓉",price:10}
 		]
+	},
+	getters:{
+		saleProducts: (state) => {
+			let saleProducts = state.products.map(
+			  product => {
+			    return {
+			      name: "**" + product.name + "**",
+			      price:product.price / 2
+			    }
+			  }
+			)
+			return saleProducts;
+		}
 	}
 })
